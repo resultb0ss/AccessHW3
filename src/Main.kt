ff5d541f365253ce075bc7dd0787bc16986fecfc
Admin <reteamboss@gmail.com>
2024-08-26 21:02:26 +0300

    Задание 3

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,15 @@
 fun main() {
-    println("Hello World!")
+    val rows = 4
+    val cols = 4
+    var num: Int = 0
+
+    val array = Array(rows) { IntArray(cols)}
+    for (row in array){
+        for(j in row.indices){
+            num++
+            row[j] = num
+        }
+
+    }
+    println(array.contentDeepToString())
 }
